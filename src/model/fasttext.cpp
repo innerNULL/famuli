e85238a1f9615a515a14e393891b17ade82d3c90
innerNULL <lyt697@outlook.com>
2021-04-07 22:38:22 +0800

    Dev backup.

---
+++ src/model/fasttext.cpp
@@ -0,0 +1,21 @@
+// file: fasttext.h      
+// date: 2021-04-06
+
+
+#ifndef MODEL_FASTTEXT_H_
+#define MODEL_FASTTEXT_H_
+
+
+#include "famuli/model/fasttext.h"
+
+
+namespace famuli {
+
+
+
+
+} // namespace famuli
+
+
+
+#endif
